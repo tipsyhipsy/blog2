f0e9588874f4875686f58a34aee06436d49e75aa
setsuto horikawa <ralralph@gmail.com>
2019-07-29 18:00:25 +0900

    add: action:ddstroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources:blogs
   resources:users
-  resources:sessions, only:[:new, :create]
+  resources:sessions, only:[:new, :create, :destroy]
 end
