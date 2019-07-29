b69e0b15b34a5d0802c6c33eeb464e7012e8e1e7
setsuto horikawa <ralralph@gmail.com>
2019-07-29 16:08:42 +0900

    add: strongparamsusers_controller, errormessage users_new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -16,5 +22,7 @@ class UsersController < ApplicationController
   end
 
   private
-
+  def user_params
+    params.require(:user).permit(:name, :mail, :password_confirmation)
+  end
 end
